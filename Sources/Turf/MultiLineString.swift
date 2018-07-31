381cab961e8aebde96ad171c60b64f48bb10d9b7
Thanos Bellos <contact@thanosbellos.com>
2018-07-31 19:27:51 +0300

    Add public initializer for LineStringFeature, MultiLineStringFeature

--- Sources/Turf/MultiLineString.swift
+++ Sources/Turf/MultiLineString.swift
@@ -8,28 +8,32 @@ import CoreLocation
  A `MultiLineString` geometry. The coordinates property represent a `[CLLocationCoordinate2D]` of two or more coordinates.
  */
 public struct MultiLineString: Codable, Equatable {
-    var type: String = GeometryType.MultiLineString.rawValue
-    public var coordinates: [[CLLocationCoordinate2D]]
+  var type: String = GeometryType.MultiLineString.rawValue
+  public var coordinates: [[CLLocationCoordinate2D]]
 }
 
 public struct MultiLineStringFeature: GeoJSONObject {
-    public var type: FeatureType = .feature
-    public var identifier: FeatureIdentifier?
-    public var geometry: MultiLineString!
-    public var properties: [String : AnyJSONType]?
-    
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: GeoJSONCodingKeys.self)
-        geometry = try container.decode(MultiLineString.self, forKey: .geometry)
-        properties = try container.decode([String: AnyJSONType]?.self, forKey: .properties)
-        identifier = try container.decodeIfPresent(FeatureIdentifier.self, forKey: .identifier)
-    }
-    
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: GeoJSONCodingKeys.self)
-        try container.encode(type, forKey: .type)
-        try container.encode(geometry, forKey: .geometry)
-        try container.encode(properties, forKey: .properties)
-        try container.encodeIfPresent(identifier, forKey: .identifier)
-    }
+  public var type: FeatureType = .feature
+  public var identifier: FeatureIdentifier?
+  public var geometry: MultiLineString!
+  public var properties: [String : AnyJSONType]?
+
+  public init(_ geometry: MultiLineString) {
+    self.geometry = geometry
+  }
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: GeoJSONCodingKeys.self)
+    geometry = try container.decode(MultiLineString.self, forKey: .geometry)
+    properties = try container.decode([String: AnyJSONType]?.self, forKey: .properties)
+    identifier = try container.decodeIfPresent(FeatureIdentifier.self, forKey: .identifier)
+  }
+
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: GeoJSONCodingKeys.self)
+    try container.encode(type, forKey: .type)
+    try container.encode(geometry, forKey: .geometry)
+    try container.encode(properties, forKey: .properties)
+    try container.encodeIfPresent(identifier, forKey: .identifier)
+  }
 }
